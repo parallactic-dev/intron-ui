1a15d294c2debf18ca13181c9977c2a04eedfcd4
Patrick Müller <patrick@parallactic.ch>
2021-01-09 17:41:07 +0100

    basic setup

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,63 @@
     <pui-icon name="apps" size="m" />
     <pui-icon name="apps" size="l" />
     <pui-icon name="apps" size="xl" />
+    <br>
+    <div>
+      <pui-button>Button Label</pui-button>
+      <pui-button type="secondary">Button Label</pui-button>
+      <pui-button type="text">Button Label</pui-button>
+    </div>
+    <br>
+    <div>
+      <pui-button icon="settings">Button Label</pui-button>
+    </div>
+    <br>
+    <div>
+      <pui-button icon="settings" type="primary" v-bind:iconOnly="true"></pui-button>
+    </div>
+
+    <br>
+    <div>
+      <pui-button size="minor">Button Label</pui-button>
+      <pui-button size="minor" type="secondary">Button Label</pui-button>
+      <pui-button size="minor" type="text">Button Label</pui-button>
+    </div>
+    <br>
+    <div>
+      <pui-button size="minor" icon="settings">Button Label</pui-button>
+    </div>
+    <br>
+    <div>
+      <pui-button size="minor" icon="settings" type="primary" v-bind:iconOnly="true"></pui-button>
+      <pui-button size="minor" icon="settings" type="text" v-bind:iconOnly="true"></pui-button>
+    </div>
   </div>
 </template>
 
 <script>
+import PuiButton from './components/PuiButton.vue';
 import PuiIcon from './components/PuiIcon';
 
 export default {
   name: 'App',
   components: {
-    PuiIcon
+    PuiIcon,
+    PuiButton
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 html {
   font-size: 10px;
 }
 
+body {
+  @include body;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
