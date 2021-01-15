5594b6735a51fc3b98e930a44ab47a671e11de08
Patrick Müller <patrick@parallactic.ch>
2021-01-15 10:17:16 +0100

    add custom icon for select dropdown

--- src/components/PuiSelect.vue
+++ src/components/PuiSelect.vue
@@ -1,18 +1,23 @@
 <template>
   <pui-form-control v-bind="$props">
-    <select class="pui-select" v-bind:class="{'pui-select--show-error': showError}" v-bind:value="value" v-on:input="onInput($event)">
-      <option v-for="option in options" v-bind:key="option.value" v-bind:value="option.value">{{ option.label }}</option>
-    </select>
+    <div class="pui-select" v-bind:class="{'pui-select--show-error': showError}">
+      <select class="pui-select__select" v-bind:value="value" v-on:input="onInput($event)">
+        <option v-for="option in options" v-bind:key="option.value" v-bind:value="option.value">{{ option.label }}</option>
+      </select>
+      <pui-icon class="pui-select__icon" name="chevron-down" />
+    </div>
   </pui-form-control>
 </template>
 
 <script>
 import PuiFormControl from './PuiFormControl';
+import PuiIcon from './PuiIcon';
 
 export default {
   name: 'PuiSelect',
   components: {
     PuiFormControl,
+    PuiIcon,
   },
   props: {
     label: {
@@ -58,20 +63,38 @@ export default {
   --c-border: #{$input-border};
   --c-focus: #{$interaction-focus};
 
+  position: relative;
+}
+
+.pui-select__select {
   @include input;
   display: block;
   width: 100%;
   box-sizing: border-box;
   padding: calc(var(--s-bezel-y) - var(--s-border)) var(--s-bezel-x);
   border: var(--s-border) solid var(--c-border);
+  // remove select arrow
+  -webkit-appearance: none;
+  -moz-appearance: none;      
+  appearance: none;
+}
+
+.pui-select__select::-ms-expand {
+  display: none;
 }
 
 .pui-select--show-error {
   --c-border: #{$input-error};
 }
 
-.pui-select:focus {
+.pui-select__select:focus {
   outline: var(--s-focus-width) solid var(--c-focus);
   z-index: 1;
+}
+
+.pui-select__icon {
+  position: absolute;
+  top: var(--s-bezel-y);
+  right: var(--s-bezel-x);
 }
 </style>
