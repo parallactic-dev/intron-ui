efdc97247eac11c0e4b5a4a1b99ba57d0a405de7
Patrick Müller <patrick@parallactic.ch>
2021-01-16 12:49:05 +0100

    add page and container classes

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,78 @@
 <template>
   <div id="app">
-    <pui-icon name="apps" size="s" />
-    <pui-icon name="apps" size="m" />
-    <pui-icon name="apps" size="l" />
-    <pui-icon name="apps" size="xl" />
+    <div class="pui-page">
+      <div class="pui-page__start"></div>
+      <main class="pui-page__main">
+        <div class="pui-container">
+
+          <div>
+            <h2 class="pui-title">Icons</h2>
+            <pui-icon name="apps" size="s" />
+            <pui-icon name="apps" size="m" />
+            <pui-icon name="apps" size="l" />
+            <pui-icon name="apps" size="xl" />
+          </div>
+
+          <div class="pui-stack pui-stack--bumblebee">
+            <h2 class="pui-title">Buttons</h2>
+            <pui-button>Button Label</pui-button>
+            <pui-button type="secondary">Button Label</pui-button>
+            <pui-button type="text">Button Label</pui-button>
+          </div>
+
+          <div class="pui-stack pui-stack--ant">
+            <pui-button icon="settings">Button Label</pui-button>
+          </div>
+          <div class="pui-stack pui-stack--ant">
+            <pui-button icon="settings" type="primary" v-bind:iconOnly="true"></pui-button>
+          </div>
+
+          <div class="pui-stack pui-stack--beetle">
+            <pui-button size="minor">Button Label</pui-button>
+            <pui-button size="minor" type="secondary">Button Label</pui-button>
+            <pui-button size="minor" type="text">Button Label</pui-button>
+          </div>
+          <div class="pui-stack pui-stack--ant">
+            <pui-button size="minor" icon="settings">Button Label</pui-button>
+          </div>
+          <div class="pui-stack pui-stack--ant">
+            <pui-button size="minor" icon="settings" type="primary" v-bind:iconOnly="true"></pui-button>
+            <pui-button size="minor" icon="settings" type="text" v-bind:iconOnly="true"></pui-button>
+          </div>
+
+          <div class="pui-stack pui-stack--bumblebee pui-stack--dragonfly@laptop pui-no-stack@desktop">
+            <div class="pui-stack pui-stack--ant">
+              <pui-input type="text" label="Input Label" />
+            </div>
+            <div class="pui-stack pui-stack--ant">
+              <pui-input type="text" label="An input with help" help-text="This is a help Text" />
+            </div>
+            <div class="pui-stack pui-stack--ant">
+              <pui-input type="email" label="An input with error" help-text="This is a help Text" error-message="Please enter a vaild email address" v-bind:show-error="true" />
+            </div>
+            <div class="pui-stack pui-stack--ant">
+              <pui-select label="Input Label" v-bind:options="[{value: 'first', label: 'First Option'}, {value: 'second', label: 'Second Option'}]" v-model="modelSelect" />
+              Model: {{modelSelect}}
+            </div>
+          </div>
+
+          <br>
+          <br>
+          <br>
+          <pui-input type="text" label="Input Label" help-text="This is a help text" size="minor" v-model="inputModel" />
+          <br>{{inputModel}}
+          <br>
+          <br>
+          <pui-select label="Input Label" size="minor" v-bind:options="[{value: 'first', label: 'First Option'}, {value: 'second', label: 'Second Option'}]" />
 
 
-    <div class="pui-stack pui-stack--bumblebee">
-      <pui-button>Button Label</pui-button>
-      <pui-button type="secondary">Button Label</pui-button>
-      <pui-button type="text">Button Label</pui-button>
-    </div>
 
-    <div class="pui-stack pui-stack--ant">
-      <pui-button icon="settings">Button Label</pui-button>
-    </div>
-    <div class="pui-stack pui-stack--ant">
-      <pui-button icon="settings" type="primary" v-bind:iconOnly="true"></pui-button>
-    </div>
-
-    <div class="pui-stack pui-stack--beetle">
-      <pui-button size="minor">Button Label</pui-button>
-      <pui-button size="minor" type="secondary">Button Label</pui-button>
-      <pui-button size="minor" type="text">Button Label</pui-button>
-    </div>
-    <div class="pui-stack pui-stack--ant">
-      <pui-button size="minor" icon="settings">Button Label</pui-button>
-    </div>
-    <div class="pui-stack pui-stack--ant">
-      <pui-button size="minor" icon="settings" type="primary" v-bind:iconOnly="true"></pui-button>
-      <pui-button size="minor" icon="settings" type="text" v-bind:iconOnly="true"></pui-button>
-    </div>
-
-    <div class="pui-stack pui-stack--bumblebee pui-stack--dragonfly@laptop pui-no-stack@desktop">
-      <div class="pui-stack pui-stack--ant">
-        <pui-input type="text" label="Input Label" />
+        </div>
+      </main>
+      <div class="pui-page__end">
+        Footer Area
       </div>
-      <div class="pui-stack pui-stack--ant">
-        <pui-input type="text" label="An input with help" help-text="This is a help Text" />
-      </div>
-      <div class="pui-stack pui-stack--ant">
-        <pui-input type="email" label="An input with error" help-text="This is a help Text" error-message="Please enter a vaild email address" v-bind:show-error="true" />
-      </div>
-      <div class="pui-stack pui-stack--ant">
-        <pui-select label="Input Label" v-bind:options="[{value: 'first', label: 'First Option'}, {value: 'second', label: 'Second Option'}]" v-model="modelSelect" />
-        Model: {{modelSelect}}
-      </div>
     </div>
-
-    <br>
-    <br>
-    <br>
-    <pui-input type="text" label="Input Label" help-text="This is a help text" size="minor" v-model="inputModel" />
-    <br>{{inputModel}}
-    <br>
-    <br>
-    <pui-select label="Input Label" size="minor" v-bind:options="[{value: 'first', label: 'First Option'}, {value: 'second', label: 'Second Option'}]" />
   </div>
 </template>
 
